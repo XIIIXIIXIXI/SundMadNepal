34431647120b7f3a4f423aa5bc8b68076700047b
zPaddyz <jul2009@live.dk>
2021-11-23 21:24:49 +0100

    Rettet lidt i HealthInfoScreen.kt

--- app/src/main/java/com/example/sundmadnepal/HealthInfoScreen.kt
+++ app/src/main/java/com/example/sundmadnepal/HealthInfoScreen.kt
@@ -1,3 +1,5 @@
+import android.media.Image
+import android.media.ImageReader
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 
@@ -9,15 +11,20 @@ import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
-
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.VerticalAlignmentLine
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 import androidx.navigation.NavController
 import com.example.sundmadnepal.R
+import org.intellij.lang.annotations.JdkConstants
+import java.util.*
 
 
 @Composable
@@ -42,61 +49,50 @@ private fun HealthScreenScaffold(
 
 @Composable
 private fun HealthScreenContent(){
-    val padding = 12.dp
-    val size = 180.dp
+
     Column() {
+        Text(text = "Health Information",
+            textAlign = TextAlign.Center,
+            fontSize = 30.sp,
+            modifier = Modifier.width(400.dp)
+        )
     Row(verticalAlignment = Alignment.CenterVertically) {
-        Image(
-            painterResource(R.drawable.adult),
-            contentDescription = "dada",
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .padding(padding)
-                .size(size)
-        )
-        Image(
-            painterResource(R.drawable.pregnant),
-            contentDescription = "dada",
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .padding(padding)
-                .size(size)
-        )
+        imageImport(image = R.drawable.adult)
+        imageImport(image = R.drawable.pregnant)
     }
         Row(){
-            Text("Hey",modifier = Modifier
-                .absolutePadding(90.dp,0.dp,75.dp))
-            Text("Hey",modifier = Modifier
-                .absolutePadding(100.dp))
+            Text("Male/Female",modifier = Modifier
+                .absolutePadding(50.dp,0.dp,50.dp))
+            Text("While pregnant",modifier = Modifier
+                .absolutePadding(60.dp))
         }
         Row(verticalAlignment = Alignment.CenterVertically) {
-        Image(
-            painterResource(R.drawable.baby),
-            contentDescription = "dada",
-            contentScale = ContentScale.Crop,
-            modifier = Modifier
-                .padding(padding)
-                .size(size)
-        )
-            Image(
-                painterResource(R.drawable.children),
-            contentDescription = "dada",
-            contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .padding(padding)
-                    .size(size)
-            )
+            imageImport(image = R.drawable.baby)
+            imageImport(image = R.drawable.children)
         }
         Row(){
-            Text("Hey",modifier = Modifier
-                .absolutePadding(90.dp,0.dp,75.dp))
-            Text("Hey",modifier = Modifier
+            Text("Baby",modifier = Modifier
+                .absolutePadding(75.dp,0.dp,75.dp))
+            Text("Children",modifier = Modifier
                 .absolutePadding(100.dp))
         }
     }
 
 }
 
+@Composable
+private fun imageImport(image: Int){
+    val padding = 12.dp
+    val size = 180.dp
+    Image(
+        painterResource(image),
+        contentDescription = "Single",
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .padding(padding)
+            .size(size)
+    )
+}
 
 @Composable
 private fun AdultScreenScaffold(
